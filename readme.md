9dbb5470d45fc48c30b7d543353d3d3d4e0cd44b
Phoenix852 <dasmon852@gmail.com>
2020-08-29 05:23:32 -0700

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,5 @@
+# DHook Tester
+
+The easiest way to test your discord webhooks
+
+The website is currently hosted on [glitch](https://dhook-tester.glitch.me/)
---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('keyup', (e) => {
+  if (e.code === "Enter") send();
+});
+
+
+function send() {
+    let url = document.getElementById('webhook-url').value;
+    let author = document.getElementById('author').value;
+    let avatar = document.getElementById('avatar-url').value;
+    let content = document.getElementById('content').value;
+  
+  if(url === "" || typeof url === 'undefined' || url === null) return alert('Webhook URL must not be empty!');
+  if(content === "" || typeof content === 'undefined' || content === null) return alert('Message content must not be empty!');
+
+
+superagent.post(url)
+    .send({content: content, username: author, avatar_url: avatar})
+    .type('form')
+    .then(function (res) {
+        console.log("Sent successfully!");
+        alert('Sent!');
+    }, function (err) {
+        alert('Error: ' + err);
+        throw err;
+    });
+}
